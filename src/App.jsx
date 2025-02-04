34f998f013a35837ed90966b972f992781f81997
Melvin Prince <67991921+melvinprince@users.noreply.github.com>
2025-02-04 04:21:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,6 @@ const AppLayout = lazy(() => import("./pages/AppLayout"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 const Login = lazy(() => import("./pages/Login"));
 
-// import HomePage from "./pages/HomePage";
-// import Product from "./pages/Product";
-// import Pricing from "./pages/Pricing";
-// import AppLayout from "./pages/AppLayout";
-// import PageNotFound from "./pages/PageNotFound";
-// import Login from "./pages/Login";
-
-// dist/assets/index-IOwSGc0b.js   564.00 kB │ gzip: 166.54 kB
-// dist/assets/index-B_f2A4RA.css   30.29 kB │ gzip:   5.03 kB
-
 function App() {
   return (
     <AuthProvider>
